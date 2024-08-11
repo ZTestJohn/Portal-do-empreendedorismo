0e94092091cbaa5ac202bedb55e1085c2a2956e7
João Vitor <ZTestJohn@users.noreply.github.com>
2024-08-11 12:09:50 -0300

    Imagens funcionais

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,27 +5,34 @@ import Banner from "../../components/Banner";
 import Container from "../../components/Container";
 import Card from "../../components/Card";
 import noticias from "../../json/db.json";
+import Category from "../../components/Category";
+import db from "../../json/db.json";
 
 
 function Home() {
+  const idMaisAcessados = [1, 50, 51, 15, 21, 22, 11]
   return (
     <>
       <Header />
       <Banner img="home.webp" />
       <Container>
-        <h1>Principais notícias</h1>
-        <div>
+        <Category title="Artigos mais acessados">
+          {
+           
+          }
+        </Category>
+        <Category title="Todos os artigos">
           {noticias.map(({ imagem_url, titulo, id }) => {
             return (
               <Card
                 img={imagem_url}
-                titulo={titulo + ' ' + id}
+                titulo={titulo}
                 key={id}
                 id={id}
               />
             );
           })}
-        </div>
+        </Category>
       </Container>
       <Footer />
     </>
