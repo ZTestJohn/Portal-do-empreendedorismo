c95fcb9cbf424b9340965e45317702f66e916833
João Vitor <ZTestJohn@users.noreply.github.com>
2024-08-12 16:14:22 -0300

    Arrumei o slider

--- src/components/SliderComponent/index.jsx
+++ src/components/SliderComponent/index.jsx
@@ -9,6 +9,33 @@ function SliderComponent({ children }) {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 990,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 670,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
   return (
     <div className="SliderConfig">
